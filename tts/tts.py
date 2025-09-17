2fc58912fe9815918bc3be761d98812305cb4930
Dean9154 <244478@student.scalda.nl>
2025-09-17 10:52:56 +0200

    Update tts.py

--- tts/tts.py
+++ tts/tts.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import wave
 from piper import PiperVoice
+import os
 
 app = Flask(__name__)
 
@@ -12,4 +13,5 @@ def speech():
     text = data.get("text")
     with wave.open("output.wav", "wb") as wav_file:
         voice.synthesize_wav(text, wav_file)
+        os.system('aplay output.wav')
     return "ok"
