f9bd79c0e4f5afaea030f7ee3c8113bea6a37898
Dean9154 <244478@student.scalda.nl>
2025-09-23 15:15:50 +0200

    Delete tts directory

--- tts/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /usr/src/app
-echo -e 'pcm.!default {\n  type plug\n  slave.pcm "dmix"\n}' > ~/.asoundrc
-python -m piper.download_voices nl_NL-ronnie-medium
-flask --app tts run --host=0.0.0.0
